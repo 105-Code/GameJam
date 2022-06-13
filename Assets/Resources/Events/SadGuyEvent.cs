694e2e4083fa1571cecf24a5f28d5ebe752eddcf
dani0105 <danielrojas0105@gmail.com>
2022-06-12 22:32:27 -0600

    Arregla ultimas observaciones

--- Assets/Resources/Events/SadGuyEvent.cs
+++ Assets/Resources/Events/SadGuyEvent.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class SadGuyEvent : EventHandler
@@ -8,6 +9,8 @@ public class SadGuyEvent : EventHandler
     private DialogComponent _dialog_component;
     public TextAsset JSONComplete;
     public LevelController levelController;
+    public GameObject bubble;
+    private GameObject _active_bubble;
 
     private void Awake()
     {
@@ -43,10 +46,22 @@ public class SadGuyEvent : EventHandler
     private void findDrain(GameObject drain)
     {
         this.draingTrigger.transform.gameObject.SetActive(false);
+        
         this._dialog_component.JSON = this.JSONComplete;
         this._dialog_component.loadFile();
         this.GetComponent<InteractComponent>().Enable = true;
+        Vector3 position = this.draingTrigger.transform.position + new Vector3(3,4,-2);
 
+        this._active_bubble = Instantiate(this.bubble, position, Quaternion.identity);
+        this._active_bubble.GetComponentInChildren<TextMeshProUGUI>().text = "Creo que por aquí se puede salir.";
+        StartCoroutine(this.hideBubble());
+
+    }
+
+    IEnumerator hideBubble()
+    {
+        yield return new WaitForSeconds(3);
+        Destroy(this._active_bubble);
     }
 
     private bool SadGuyComplete()
